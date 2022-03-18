be03b7c9012c03354aaac1f93ced247869ca10b6
RichardPickman <drezzerock@gmail.com>
2022-03-18 19:39:50 +0300

    fix: remove unnecessary code

--- src/map/map.test.ts
+++ src/map/map.test.ts
@@ -2,13 +2,11 @@ import { map } from '.';
 
 
 describe('map', () => {
-    describe(('with initialState'), () => {
-        test('Should return: valid result', () => {
-            const result = map((item: number) => item + 1, [12,34,56,78]);
+    test('Should return: valid result', () => {
+        const result = map((item: number) => item + 1, [12,34,56,78]);
 
-            const expectedResult = [13,35,57,79];
+        const expectedResult = [13,35,57,79];
 
-            expect(result).toEqual(expectedResult);
-        });
+        expect(result).toEqual(expectedResult);
     });
 });
