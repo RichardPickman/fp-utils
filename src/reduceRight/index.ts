475db304a6e8e7c4f16fbbf573b680130c1cef0a
RichardPickman <drezzerock@gmail.com>
2022-03-19 18:57:54 +0100

    refactor: remove overhead variable

--- src/reduceRight/index.ts
+++ src/reduceRight/index.ts
@@ -1,9 +1,8 @@
 export function reduceRight<T, TResult>(callback: (acc: TResult, item: T, index: number, items: T[]) => TResult, items: T[], initialState?: TResult): TResult {
     const hasInitialState = typeof initialState !== 'undefined';
-    const itemsIndex = items.length;
-    let result = hasInitialState ? initialState : (items[itemsIndex - 1] as unknown as TResult);
+    let result = hasInitialState ? initialState : (items[items.length - 1] as unknown as TResult);
 
-    for (let i = hasInitialState ? itemsIndex - 1 : itemsIndex - 2; i > -1; i--) {
+    for (let i = hasInitialState ? items.length - 1 : items.length - 2; i > -1; i--) {
         result = callback(result, items[i], i, items);
     }
 
