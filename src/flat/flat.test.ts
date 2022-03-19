ce44c7700b2b50dfbe65c7be2ad87f8c7e726dd9
Richard Pickman <drezzerock@gmail.com>
2022-03-19 23:46:58 +0100

    feat: flat function

---
+++ src/flat/flat.test.ts
@@ -0,0 +1,18 @@
+import { flat } from '.';
+
+
+describe('flat', () => {
+    test('Should return: valid result for nested array', () => {
+        const result = flat([1, [1, [1]], 1]);
+        const expectedResult = [1, 1, 1, 1];
+
+        expect(result).toEqual(expectedResult);
+    });
+    
+    test('Should return: valid result for flat array', () => {
+        const result = flat([1, [1, [1]], 1]);
+        const expectedResult = [1, 1, 1, 1];
+
+        expect(result).toEqual(expectedResult);
+    });
+});
---
+++ src/flat/index.ts
@@ -0,0 +1,14 @@
+export function flat<T>(array: any): T[] {
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            const flatteredArray = flat(array[i])
+            result.push(...flatteredArray)
+        } else {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
