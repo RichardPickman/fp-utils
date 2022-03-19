fd11c71f785e2b82f568984590c2b162f9651d54
RichardPickman <drezzerock@gmail.com>
2022-03-19 18:51:10 +0100

    fix: reduceRight without reverse

--- src/reduceRigth/index.ts
+++ src/reduceRigth/index.ts
@@ -1,12 +1,10 @@
-import { reverse } from "../reverse";
-
 export function reduceRigth<T, TResult>(callback: (acc: TResult, item: T, index: number, items: T[]) => TResult, items: T[], initialState?: TResult): TResult {
     const hasInitialState = typeof initialState !== 'undefined';
-    const reverseItems = reverse(items);
-    let result = hasInitialState ? initialState : (reverseItems[0] as unknown as TResult);
+    const itemsIndex = items.length;
+    let result = hasInitialState ? initialState : (items[itemsIndex - 1] as unknown as TResult);
 
-    for (let i = hasInitialState ? 0 : 1; i < reverseItems.length; i++) {
-        result = callback(result, reverseItems[i], i, reverseItems);
+    for (let i = hasInitialState ? itemsIndex - 1 : itemsIndex - 2; i > -1; i--) {
+        result = callback(result, items[i], i, items);
     }
 
     return result;
