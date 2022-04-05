c21ff19abdbd743a5a86584057c35e5dbd14694b
RichardPickman <drezzerock@gmail.com>
2022-04-06 01:25:43 +0200

    refactor: change algorithm

--- src/omit/index.ts
+++ src/omit/index.ts
@@ -1,11 +1,11 @@
-function omit(obj: {}, variables: string[]): {} {
-    const result = new Object;
-    const keys = Object.keys(obj)
+function omit(obj: { [k: string]: unknown }, variables: string[]) {
+    const result: Partial<typeof obj> = {};
+    const keys = Object.keys(obj);
 
-    for (let i = 0; i < keys.length; i++) {
-        if (variables.indexOf(keys[i]) > -1) continue;
-        // @ts-ignore
-        else result[keys[i]] = obj[keys[i]];
+    for (const key of keys) {
+        if (!variables.includes(key)) {
+            result[key] = obj[key];
+        }
     }
 
     return result;
--- src/omit/omit.test.ts
+++ src/omit/omit.test.ts
@@ -1,37 +1,37 @@
 import omit from '.';
 
 describe('omit', () => {
-    test('Should return: valid result', () => {
+    test('Should return valid result', () => {
         const obj = {
             key: 'value',
             index: 0,
             position: 5,
-        }
+        };
 
-        const result = omit(obj, ['index'])
+        const result = omit(obj, ['index']);
 
         const expectedResult = {
             key: 'value',
             position: 5,
-        }
+        };
 
         expect(result).toEqual(expectedResult);
     });
 
-    test('Should return: valid result with empty array', () => {
+    test('Should return valid result with empty array', () => {
         const obj = {
             key: 'value',
             index: 0,
             position: 5,
-        }
+        };
 
-        const result = omit(obj, [])
+        const result = omit(obj, []);
 
         const expectedResult = {
             key: 'value',
             index: 0,
             position: 5,
-        }
+        };
 
         expect(result).toEqual(expectedResult);
     });
