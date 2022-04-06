f428e9fda74fbea3af34e0bce08457c2a9d3fef5
RichardPickman <drezzerock@gmail.com>
2022-04-06 16:08:57 +0200

    refactor: change naming of arguments

--- src/omit/index.ts
+++ src/omit/index.ts
@@ -1,9 +1,9 @@
-function omit(obj: { [k: string]: unknown }, variables: string[]) {
+function omit(obj: { [k: string]: unknown }, names: string[]) {
     const result: Partial<typeof obj> = {};
     const keys = Object.keys(obj);
 
     for (const key of keys) {
-        if (!variables.includes(key)) {
+        if (!names.includes(key)) {
             result[key] = obj[key];
         }
     }
